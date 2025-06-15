b9dc6b8da073735658ca835ee48a878548391500
Baptiste Dangy <baptiste1.dangy@gmail.com>
2025-06-15 15:09:32 +0200

    Initial Commit

---
+++ Moovie/MoovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoovieApp.swift
+//  Moovie
+//
+//  Created by Baptiste Dangy on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoovieTests/MoovieTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoovieTests.swift
+//  MoovieTests
+//
+//  Created by Baptiste Dangy on 15/06/2025.
+//
+
+import Testing
+
+struct MoovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
